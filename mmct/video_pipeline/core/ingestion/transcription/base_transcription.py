0c47ea72c077b9458be3e5ebd3c7f1b07ecf1537
Aman Patkar <apatkar726@gmail.com>
2025-11-18 08:14:17 +0000

    removed glossary

--- mmct/video_pipeline/core/ingestion/transcription/base_transcription.py
+++ mmct/video_pipeline/core/ingestion/transcription/base_transcription.py
@@ -14,9 +14,3 @@ class Transcription:
             self.source_language = {'lang':None,'lang-code':None}
         else:
             self.source_language = {'lang':language.name,'lang-code':language.value}
-        self.load_glossary()
-        logger.info("Glossary Loaded")
-        
-    def load_glossary(self):
-        self.glossary_df = pd.read_csv(os.path.join(os.path.dirname(os.path.dirname(__file__)),r"Glossary.csv"))
-        self.hindi_glossary = self.glossary_df['hindi_terms'].to_list()
