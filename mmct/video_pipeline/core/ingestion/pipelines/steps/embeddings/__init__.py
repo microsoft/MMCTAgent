50a49d41962660334e0a294723800f79142c3aa7
Aman <apatkar726@gmail.com>
2026-04-07 11:49:24 +0000

    refactor(mmct/video_pipeline): remove deprecated chapter_enrichment, chapters, embeddings, keyframes, upload step modules

--- mmct/video_pipeline/core/ingestion/pipelines/steps/embeddings/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Embedding generation step."""
-
-from .embedding_orchestrator import EmbeddingOrchestrator
-
-__all__ = ["EmbeddingOrchestrator"]
--- mmct/video_pipeline/core/ingestion/pipelines/steps/upload/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Upload and indexing step."""
-
-from .upload_orchestrator import UploadOrchestrator
-
-__all__ = ["UploadOrchestrator"]
